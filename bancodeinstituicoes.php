76b5dc79b77ea8fac426ab28abd565daba29f797
Afonso Figueira <afonsoxd98@gmail.com>
2021-05-31 19:43:49 +0100

    Main commit
    website

---
+++ bancodeinstituicoes.php
@@ -0,0 +1,508 @@
+<!DOCTYPE html>
+<!-- saved from url=(0093)https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><script src="./bancodeinstituicoes.php_files/analytics.js.download" type="text/javascript"></script>
+<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app39.us.archive.org';v.server_ms=404;archive_analytics.send_pageview({});});</script><script type="text/javascript" src="./bancodeinstituicoes.php_files/playback.bundle.js.download" charset="utf-8"></script>
+<script type="text/javascript" src="./bancodeinstituicoes.php_files/wombat.js.download" charset="utf-8"></script>
+<script type="text/javascript">
+  __wm.init("https://web.archive.org/web");
+  __wm.wombat("http://casadovoluntario.pt/bancodeinstituicoes.php","20161013031201","https://web.archive.org/","web","/_static/",
+	      "1476328321");
+</script>
+<link rel="stylesheet" type="text/css" href="./bancodeinstituicoes.php_files/banner-styles.css">
+<link rel="stylesheet" type="text/css" href="./bancodeinstituicoes.php_files/iconochive.css">
+<!-- End Wayback Rewrite JS Include -->
+
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="HTML">
+    <meta name="description" content="Website para instituição">
+    <meta name="copyright" content="O meu website">
+    <meta name="author" content="Érica Freitas, al214079@epcc.pt">
+    
+	
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="./bancodeinstituicoes.php_files/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="./bancodeinstituicoes.php_files/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="./bancodeinstituicoes.php_files/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="./bancodeinstituicoes.php_files/AdminLTE.min.css">
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+          page. However, you can choose any other skin. Make sure you
+          apply the skin class to the body tag so the changes take effect.
+    -->
+    <link rel="stylesheet" href="./bancodeinstituicoes.php_files/skin-blue.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<link rel="shortcut icon" href="https://web.archive.org/web/20161013031201im_/http://casadovoluntario.pt/img/favicon.jpg" type="image/jpg">
+    <link rel="stylesheet" href="./bancodeinstituicoes.php_files/font-awesome.min(1).css">
+    <title>Casa do Voluntário</title>
+    <link href="./bancodeinstituicoes.php_files/bootstrap.min(1).css" rel="stylesheet">
+    <link href="./bancodeinstituicoes.php_files/newstyle.css" rel="stylesheet">
+    <link href="./bancodeinstituicoes.php_files/tabela.css" rel="stylesheet">
+	<link href="./bancodeinstituicoes.php_files/css" rel="stylesheet" type="text/css">
+	<link href="./bancodeinstituicoes.php_files/css(1)" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="./bancodeinstituicoes.php_files/bootstrap.min(2).css">
+  </head>
+  <body data-new-gr-c-s-check-loaded="14.1012.0" data-gr-ext-installed="" class="modal-open"><!-- BEGIN WAYBACK TOOLBAR INSERT -->
+<style type="text/css">
+body {
+  margin-top:0 !important;
+  padding-top:0 !important;
+  /*min-width:800px !important;*/
+}
+</style>
+<script>__wm.rw(0);</script>
+<div id="wm-ipp-base" lang="en" style="display: block; direction: ltr;">
+</div><div id="wm-ipp-print">The Wayback Machine - https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php</div>
+<div id="donato" style="position:relative;width:100%;">
+  <div id="donato-base">
+    <iframe id="donato-if" src="./bancodeinstituicoes.php_files/donate.html" scrolling="no" frameborder="0" style="width:100%; height:100%">
+    </iframe>
+  </div>
+</div><script type="text/javascript">
+__wm.bt(650,27,25,2,"web","http://casadovoluntario.pt/bancodeinstituicoes.php","20161013031201",1996,"/_static/",["/_static/css/banner-styles.css?v=omkqRugM","/_static/css/iconochive.css?v=qtvMKcIJ"], false);
+  __wm.rw(1);
+</script>
+<!-- END WAYBACK TOOLBAR INSERT -->
+  
+  
+	<div class="modal fade in" id="modal" tabindex="-1" role="dialog" data-backdrop="static" aria-hidden="false" style="display: block;">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+				<a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/index.html">
+					<button type="button" class="close"><span aria-hidden="true">×</span></button>
+				</a>
+					<h3 class="modal-title" id="myModalLabel" style="color: #be2c2c; font-family: Candal">Banco de Instituições</h3>
+				</div>
+				<div class="modal-body">
+				
+					<div class="row">
+						<div class="panel-group" id="accordion">
+						
+						  <div class="panel panel-default">
+							<div class="panel-heading" style="background-color: #be2c2c; color: white;">
+							  <h4 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion" href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#p1" style="font-family: Candal" class="" aria-expanded="true">Banco de Instituições</a>
+							  </h4>
+							</div>
+							<div id="p1" class="panel-collapse collapse in" aria-expanded="true" style="">
+							  <div class="panel-body">
+								<div class="row">
+									<div style="margin-left: 15px; margin-right: 15px; color:#193984; font-family: Roboto">
+									
+										<!-- Content Wrapper. Contains page content -->
+										  <div>
+										  
+										  
+											<!-- Content Header (Page header) -->
+											
+
+											<!-- Main content -->
+											<section class="content">
+          <!-- Your Page Content Here -->
+			<div class="box">
+                
+                <div class="box-body">
+                  <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="example1_length"></div></div><div class="col-sm-6"><div id="example1_filter" class="dataTables_filter"></div></div></div><div class="row"><div class="col-sm-12"><div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="example1"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
+                    <thead>
+                      <tr role="row" style="color: #be2c2c"><th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Nome: activate to sort column descending" style="width: 142px;" aria-sort="ascending">Nome</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Concelho: activate to sort column ascending" style="width: 167px;">Concelho</th></tr>
+                    </thead>
+                    <tbody>
+					
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+										
+															<tr role="row" class="odd">
+											<td class="sorting_1">AAAIDD - GDD Dançando com a Diferença</td>
+											<td>Funchal</td>
+										</tr><tr role="row" class="even">
+											<td class="sorting_1">Abraço - Associação de Apoio a pessoas com HIV/SIDA</td>
+											<td>Funchal</td>
+										</tr><tr role="row" class="odd">
+											<td class="sorting_1">Abrigo de Nossa Senhora de Fátima</td>
+											<td>Funchal</td>
+										</tr><tr role="row" class="even">
+											<td class="sorting_1">ACAPORAMA - Associação das Casas do Povo da Região Autónoma da Madeira</td>
+											<td>Funchal</td>
+										</tr><tr role="row" class="odd">
+											<td class="sorting_1">ACCE - Madeira - Associação Cultural Caminho da Europa</td>
+											<td>Funchal</td>
+										</tr><tr role="row" class="even">
+											<td class="sorting_1">Acreditar</td>
+											<td>Funchal</td>
+										</tr><tr role="row" class="odd">
+											<td class="sorting_1">ADBRAVA - Associação de Desenvolvimento da Ribeira Brava</td>
+											<td>Ribeira Brava</td>
+										</tr><tr role="row" class="even">
+											<td class="sorting_1">ADENORMA - Associação Desenvolvimento Costa Norte Madeira</td>
+											<td>São Vicente</td>
+										</tr><tr role="row" class="odd">
+											<td class="sorting_1">ADESCA - Associação de Desenvolvimento Social e Cultural da Camacha</td>
+											<td>Santa Cruz</td>
+										</tr><tr role="row" class="even">
+											<td class="sorting_1">AFARAM - Associação dos Familiares e Amigos dos Utentes da Casa S. João de Deus</td>
+											<td>Funchal</td>
+										</tr></tbody>
+                    <tfoot style="color: #be2c2c">
+                      <tr><th rowspan="1" colspan="1">Nome</th><th rowspan="1" colspan="1">Concelho</th></tr>
+                    </tfoot>
+                  </table></div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 129 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="example1_previous"><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button active"><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="1" tabindex="0">1</a></li><li class="paginate_button "><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="2" tabindex="0">2</a></li><li class="paginate_button "><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="3" tabindex="0">3</a></li><li class="paginate_button "><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="4" tabindex="0">4</a></li><li class="paginate_button "><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="5" tabindex="0">5</a></li><li class="paginate_button disabled" id="example1_ellipsis"><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="6" tabindex="0">…</a></li><li class="paginate_button "><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="7" tabindex="0">13</a></li><li class="paginate_button next" id="example1_next"><a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/bancodeinstituicoes.php#" aria-controls="example1" data-dt-idx="8" tabindex="0">Next</a></li></ul></div></div></div></div></div></div></div>
+                </div><!-- /.box-body -->
+              </div>
+</section>											<!-- /.content -->
+										  </div><!-- /.content-wrapper -->
+										 
+									
+										
+										
+										<a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/formulario_i.html">
+											<button type="button" class="btn btn-default" style="font-family: Candal; color:#193984">Ficha de Inscrição</button>
+										</a>
+									</div>
+								</div>
+							  </div>
+							</div>
+						  </div>
+						</div>        
+					</div>
+				</div>
+				<div class="modal-footer">
+					<a href="https://web.archive.org/web/20161013031201/http://casadovoluntario.pt/index.html">
+						<button type="button" class="btn btn-default" style="font-family: Candal; color:#193984">Voltar</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+  
+	
+    <script src="./bancodeinstituicoes.php_files/bootstrap.min.js.download"></script>
+	<script src="./bancodeinstituicoes.php_files/jquery.min.js.download"></script>
+	<script src="./bancodeinstituicoes.php_files/bootstrap.min.js(1).download"></script>
+	<script>
+		$(document).ready(function(){
+			$(window).load(function(){
+				$("#modal").modal("show");
+			});
+		});
+	</script>
+  <!-- REQUIRED JS SCRIPTS -->
+
+    <!-- jQuery 2.1.4 -->
+    <script src="./bancodeinstituicoes.php_files/jQuery-2.1.4.min.js.download"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="./bancodeinstituicoes.php_files/bootstrap.min.js(2).download"></script>
+    <!-- AdminLTE App -->
+    <script src="./bancodeinstituicoes.php_files/app.min.js.download"></script>
+	<script src="./bancodeinstituicoes.php_files/jquery.dataTables.min.js.download"></script>
+	<script src="./bancodeinstituicoes.php_files/dataTables.bootstrap.min.js.download"></script>
+	
+	
+	<script>
+	
+	$(function () {
+        
+		$('#example1').DataTable({ <!-- O ID DA TABELA!!!!!! -->
+		  "language": {
+			"url": "json/portuguese.json"
+		  }
+		});
+		
+		
+        $('#example2').DataTable({
+          "paging": true,
+          "lengthChange": false,
+          "searching": false,
+          "ordering": true,
+          "info": true,
+          "autoWidth": false
+        });
+		
+	});
+	  
+    </script>
+	
+    <!-- Optionally, you can add Slimscroll and FastClick plugins.
+         Both of these plugins are recommended to enhance the
+         user experience. Slimscroll is required when using the
+         fixed layout. -->
+  
+
+<!--
+playback timings (ms):
+  PetaboxLoader3.datanode: 130.12 (4)
+  captures_list: 259.835
+  PetaboxLoader3.resolve: 202.259 (3)
+  CDXLines.iter: 16.765 (3)
+  load_resource: 130.81
+  exclusion.robots: 0.288
+  exclusion.robots.policy: 0.278
+  LoadShardBlock: 230.768 (3)
+  esindex: 0.01
+  RedisCDXSource: 9.673
+--><div class="modal-backdrop fade in"></div></body></html>
